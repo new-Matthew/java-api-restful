fa2b1a64ba6fc656ecb40a8957ce0d9e6bd7867e
matheus <matheusrodrig16@gmail.com>
2024-12-17 11:36:31 -0300

    feat: get product by id

--- src/main/java/com/teste/primeiro_exemplo/controller/ProductController.java
+++ src/main/java/com/teste/primeiro_exemplo/controller/ProductController.java
@@ -2,12 +2,15 @@ package com.teste.primeiro_exemplo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.teste.primeiro_exemplo.model.Product;
 import java.util.List;
+import java.util.Optional;
+
 import com.teste.primeiro_exemplo.services.ProductService;
 
 @RestController
@@ -21,6 +24,12 @@ public class ProductController {
     public List<Product> getAll(){
         return productService.getAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getById(@PathVariable Integer id){
+        return productService.getById(id);
+    }
+
     @PostMapping
     public Product toAdd(@RequestBody Product product){ // convete em produto (biding)
         return productService.toAdd(product);

71a0ee0856135204d014e9c6229020e82cc160a1
matheus <matheusrodrig16@gmail.com>
2024-12-19 17:29:34 -0300

    feat: Initializing DTO

---
+++ src/main/java/com/teste/primeiro_exemplo/shared/productDTO.java
@@ -0,0 +1,58 @@
+package com.teste.primeiro_exemplo.shared;
+
+public class productDTO { // desacopla model de repository
+
+    //#region Atributes
+    private Integer id;
+
+    private String name;
+
+    private Integer amount;
+
+    private Double value;
+
+    private String note;
+    //#endregion
+
+    //#region Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    //#endregion 
+}
